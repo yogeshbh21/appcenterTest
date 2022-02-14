3aeb94d0cead155a9d937868d8706cc43e93ab4a
impirios <helloworldwastaken@gmail.com>
2022-02-14 12:06:47 +0530

    initial commit

---
+++ android/app/src/main/java/com/impirios/newapp/MainActivity.java
@@ -0,0 +1,5 @@
+package com.impirios.newapp;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.impirios.newapp',
+  appName: 'angular.io-example',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
